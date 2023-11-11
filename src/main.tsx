491fedde966a1adadea91d5b02d1d4ac16f6b559
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2023-11-12 01:55:20 +0800

    feat: add styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,9 @@ import { StrictMode } from 'react'
 
 import { createRoot } from 'react-dom/client'
 
-import './index.css'
+import './styles/index.scss'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
 
 // eslint-disable-next-line import/extensions
 import { App } from './App.tsx'
