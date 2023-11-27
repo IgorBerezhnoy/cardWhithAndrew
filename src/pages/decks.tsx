fc76df6cbf06aa62f2b0e0367d349e25bf4a5015
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2023-11-28 03:06:00 +0800

    stopped at  2.53

--- src/pages/decks.tsx
+++ src/pages/decks.tsx
@@ -1,11 +1,53 @@
-import { useGetDecksQuery } from '@/services/base-api'
+import { useState } from 'react'
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeadCell,
+  TableRow,
+} from '@/components/ui/table'
+import { Typography } from '@/components/ui/typography'
+import { useGetDeckByIdQuery, useGetDecksQuery } from '@/services/base-api'
 
 export const Decks = () => {
-  const { data, isError, isLoading } = useGetDecksQuery()
+  const [currentPage, setCurrentPage] = useState(1)
+  const { data, error, isLoading } = useGetDecksQuery()
+  const { data: deckById } = useGetDeckByIdQuery({ id: 'clpgfu8fh054cwv2qx449vy3g' })
 
-  console.log(data)
-  console.log(isError)
-  console.log(isLoading)
+  console.log(deckById)
+  if (isLoading) {
+    return <Typography as={'h1'}>Loading</Typography>
+  }
+  if (error) {
+    return <Typography as={'h1'}>Error</Typography>
+  }
 
-  return <div>decks</div>
+  return (
+    <div>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableHeadCell>Name</TableHeadCell>
+            <TableHeadCell>Cards</TableHeadCell>
+            <TableHeadCell>Updated</TableHeadCell>
+            <TableHeadCell>Author</TableHeadCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data?.items.map(deck => {
+            return (
+              <TableRow key={deck.id}>
+                <TableCell>{deck?.name}</TableCell>
+                <TableCell>{deck?.cardsCount}</TableCell>
+                <TableCell>{new Date(deck?.updated).toLocaleDateString()}</TableCell>
+                <TableCell>{deck?.author.name}</TableCell>
+              </TableRow>
+            )
+          })}
+        </TableBody>
+      </Table>
+    </div>
+  )
 }
